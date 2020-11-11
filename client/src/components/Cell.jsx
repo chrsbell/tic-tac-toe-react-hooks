486119bd32b0cfb641505595de3dd6d7cfc4ba74
Chris Bell <chrisbell.dev@gmail.com>
2020-11-10 17:33:28 -0800

    Change Game State on Cell click

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -1,26 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import { GameContext } from './GameContext.jsx';
 
 const Container = styled.div`
+  text-align: center;
+  vertical-align: middle;
   display: inline-block;
   box-sizing: border-box;
   width: 120px;
   height: 120px;
   border: 2px solid #969696;
   background: #d9dbda;
-  margin: 10px;
 `;
 
 const Cell = () => {
   const { dispatch, gameState } = useContext(GameContext);
+  const [innerText, setInnerText] = useState(' ');
 
   const onClick = () => {
-    dispatch({ type: 'score', score: { playerOne: 1, playerTwo: 2 } });
+    // change the player's turn
+    setInnerText(gameState.turn === 1 ? 'X' : 'O');
+    dispatch({ type: 'turn', turn: gameState.turn === 1 ? 2 : 1 });
   };
 
-  return <Container onClick={onClick}>{` `}</Container>;
+  return <Container onClick={onClick}>{innerText}</Container>;
 };
 
 export default Cell;
